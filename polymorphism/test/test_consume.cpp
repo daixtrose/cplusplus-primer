a6615548de29cf6e534cb16433ab1e57fa85f150
Markus Werle <daixtrose@users.noreply.github.com>
2024-10-16 16:34:43 +0200

    Add bdd test example

--- polymorphism/test/test_consume.cpp
+++ polymorphism/test/test_consume.cpp
@@ -5,11 +5,10 @@
 #include <polymorphism/impl_with_interface.hpp>
 #include <polymorphism/impl_without_interface.hpp>
 
-// ctest --rerun-failed --output-on-failure
-
 int main()
 {
     using namespace boost::ut;
+    using namespace boost::ut::bdd;
     using namespace std::string_literals;
 
     "classic interface call yields 42"_test = [] {
@@ -24,4 +23,24 @@ int main()
         expect("The answer to all questions is 42"s == modern::consume(i));
         expect("42"s == i.coolFeature());
     };
+
+    "[modern]"_test
+        = [] {
+              given("I have a an implementation that adheres to a concept") = [] {
+                  modern::Impl impl;
+                  expect("<default value>"s == impl.coolFeature());
+
+                  when("I pass it to a function that expects an argument that fulfils the constraints") = [&] {
+                      auto result = modern::consume(impl);
+
+                      then("the answer to all questions should be given") = [=] {
+                          expect("The answer to all questions is 42"s == result);
+                      };
+
+                      then("the state of the argument should be modified as a side effect") = [=] {
+                          expect("42"s == impl.coolFeature());
+                      };
+                  };
+              };
+          };
 }
