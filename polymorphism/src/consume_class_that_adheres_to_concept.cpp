55afa89b223f0f6f45a3bdd5b5db542bcf1d0f53
Markus Werle <daixtrose@users.noreply.github.com>
2024-10-16 22:27:10 +0200

    fix typo

--- polymorphism/src/consume_class_that_adheres_to_concept.cpp
+++ polymorphism/src/consume_class_that_adheres_to_concept.cpp
@@ -1,10 +1,9 @@
+#include <consume_class_that_adheres_to_concept.ipp>
 #include <polymorphism/impl_without_interface.hpp>
-
-#include <../src/consume_class_that_adheres_to_concept.ipp>
 
 namespace modern {
 
-// explicit template instatiation
+// Explicit template instatiation
 template std::string consume<Impl>(Impl&);
 
 }
